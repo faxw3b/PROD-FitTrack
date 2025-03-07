cd1dee06c06a6e14cd59a8657ccbb8963312212c
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2025-03-07 19:21:15 +0300

    Update api key ImgBB with prefix VITE_

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const IMGBB_API_KEY = import.meta.env.IMGBB_API_KEY as string;
+export const IMGBB_API_KEY = import.meta.env.VITE_IMGBB_API_KEY as string;
 export const IMGBB_API_URL = 'https://api.imgbb.com';
 export const MAX_FILE_SIZE = 30 * 1024 * 1024;
